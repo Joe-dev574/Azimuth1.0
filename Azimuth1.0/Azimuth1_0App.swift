d924e8565a0ef3371b9204506daba828f291358e
JDeWeese <jdeweese574@icloud.com>
2024-09-15 10:55:10 -0500

    Initial Commit

---
+++ Azimuth1.0/Azimuth1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Azimuth1_0App.swift
+//  Azimuth1.0
+//
+//  Created by Joseph DeWeese on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Azimuth1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Azimuth1.0UITests/Azimuth1_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Azimuth1_0UITestsLaunchTests.swift
+//  Azimuth1.0UITests
+//
+//  Created by Joseph DeWeese on 9/15/24.
+//
+
+import XCTest
+
+final class Azimuth1_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
